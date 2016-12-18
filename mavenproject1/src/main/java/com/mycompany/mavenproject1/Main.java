f18b9204a93d6e46e9c5c1952af920a77531d7fe
yuval klein <yuval@minutelab.io>
2016-12-18 14:15:02 +0200

    created maven project

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Main.java
@@ -0,0 +1,11 @@
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Your name
+ */
+public class Main {
+    public static void main(String... param) {
+        System.out.println("Starting application...");
+    }
+}
